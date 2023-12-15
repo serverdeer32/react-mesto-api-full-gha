4273f7607dd72840893cddb13f1344c06568058a
Alexey Kuznetsov <i@akzntsv.ru>
2023-12-16 02:05:19 +0300

    повторная авторизация

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -90,6 +90,7 @@ function App() {
     if (localStorage.jwt) {
       auth.getContent(localStorage.jwt)
         .then(res => {
+          console.log(res)
           setUserData(res.email)
           setLoggedIn(true)
           navigate('/')
