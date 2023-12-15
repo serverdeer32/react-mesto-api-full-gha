a8a4c1ca2c97259a42d5944b560ae0786d11779b
Alexey Kuznetsov <i@akzntsv.ru>
2023-12-15 13:17:58 +0300

    мелкие правки

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,12 @@ const signupRouter = require('./signup');
 const signinRouter = require('./signin');
 const auth = require('../middlewares/auth');
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); 
+
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
 
