2e7ed077ffdaefa7ded7d78d0c76c3812b128b2d
Alexey Kuznetsov <i@akzntsv.ru>
2023-12-16 02:56:29 +0300

    crash test

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger')
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
