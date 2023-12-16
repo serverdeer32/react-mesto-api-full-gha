38fad2425fc11fabfc4b19b6d8ffd3bfe2b27fba
Alexey Kuznetsov <i@akzntsv.ru>
2023-12-16 12:06:50 +0300

    crash test

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,12 @@ mongoose.connect(DB_URL, {
 app.use(requestLogger);
 app.use(limiter);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); 
+
 app.use('/', require('./routes/index'));
 
 app.all('*', (req, res) => {
